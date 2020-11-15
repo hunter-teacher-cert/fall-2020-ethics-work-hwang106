d436526ccdd59a11f1b1e48d2e24266bc783a054
Huan Wang <huan.w.wang@gmail.com>
2020-11-15 17:19:11 -0500

    added vote tallying method, and started implementing a simulation-based exploration of example-space, rather than the deterministic one in le district, mainly because I didn't know how to alogrithmically generate all possible districts

--- 08-gerry/08-gerry.py
+++ 08-gerry/08-gerry.py
@@ -3,7 +3,7 @@ import random
 
 rows = 6
 cols = 3
-state_name = "PR"
+total_districts = 6
 
 def make_state(rows, cols):
     state = [[0 for x in range(cols)] for y in range(rows)]
@@ -18,8 +18,16 @@ def rand_votes(state):
         for y in range(cols):
             state[x][y] = random.randint(0,1)
 
+def rand_thirded_votes(state):
+    counter = 0
+    while counter < ((rows * cols) / 3):
+        a = random.randint(0, rows - 1)
+        b = random.randint(0, cols - 1)
+        if state[a][b] != 1: 
+            state[a][b] = 1
+            counter += 1
+
 def create_districts(state):
-    total_districts = 6
     districts = [[] for y in range(total_districts)] #initializes district list
     remaining_coordinates = [[0,0] for x in range(rows*cols)] #initializes master list of coordinates
     counter = 0
@@ -35,9 +43,9 @@ def create_districts(state):
         if counter%cols == 0:
             current_row += 1
 
-    print("This is the initial master list of all coordinates")
-    print(remaining_coordinates)
-    print("\n")
+    #print("This is the initial master list of all coordinates")
+    #print(remaining_coordinates)
+    #print("\n")
     #function to append a cell to a district's list and remove it from the master remaining list
     def update_cell(district_number, cell): 
         remaining_coordinates.remove(cell)
@@ -87,11 +95,11 @@ def create_districts(state):
     
     for x in range(total_districts):
         first_district_cell(x)
-
+    '''
     print("This is the list of remaining coordinates")
     print(remaining_coordinates)
     print("\n")
-
+    '''
     while len(remaining_coordinates) != 0:
         for x in range(total_districts):
             list_of_adjacent = check_adjacent(x)
@@ -100,11 +108,11 @@ def create_districts(state):
                 chosen_adjacent_cell_index = random.randint(0,len(list_of_adjacent_remaining)-1)
                 chosen_adjacent_cell = list_of_adjacent_remaining[chosen_adjacent_cell_index]
                 update_cell(x, chosen_adjacent_cell)
-
+            '''
             print("This is the list of remaining adjacent cells for District " + str(x+1))
             print(list_of_adjacent_remaining)
             print("\n")
-    
+            '''
 
 
 
@@ -124,19 +132,67 @@ def show_districts(districts):
         print("District " + str(d))
         print(i)
         d += 1
-'''
+
 def count_votes(state, districts):
-    for       
-'''
+    vote_tally = []
+    for x in range(total_districts):
+        current_tally = 0
+        for y in range(len(districts[x])):
+            if state[districts[x][y][1]][districts[x][y][0]] == 1:
+                current_tally += 1
+        vote_tally.append(current_tally)
+    #print(vote_tally)
+    return vote_tally
+
+def count_wins(vote_tally):
+    win_num = 0
+    for x in range(len(vote_tally)):
+        if vote_tally[x]/len(districts[x]) > .5:
+            win_num += 1
+    #print(win_num)
+    return win_num
+
+
+def check_district_size(districts):
+    for x in range(total_districts):
+        # Checks to see if district in question has geographically-balanced number of cells
+        if len(districts[x]) != (rows * cols)/total_districts:
+            return False
+    return True
+
+
+
 #Main
 state_grid = make_state(rows, cols)
+#show_state(state_grid)
+rand_thirded_votes(state_grid)
+#print("\n")
 show_state(state_grid)
-rand_votes(state_grid)
+print("\n")
+#Computationally lazy and inefficient way of ensuring equal-sized districts by repeating create_districts algorithm until balanced
+num_district_sims = 200
+win_tally = [0 for x in range(total_districts + 1)]
+for x in range(num_district_sims):
+    is_balanced = False
+    while not is_balanced:
+        districts = create_districts(state_grid)
+        is_balanced = check_district_size(districts)
+    print("\n")
+    show_districts(districts)
+    print("\n")
+    vote_tally = count_votes(state_grid, districts)
+    print("\n")
+    num_wins = count_wins(vote_tally)
+    win_tally[num_wins] += 1
+    print(win_tally)
+    win_freq = [y / (x+1) for y in win_tally]
+    print(win_freq)
+
 print("\n")
 show_state(state_grid)
-print("\n")
-districts = create_districts(state_grid)
-print("\n")
-show_districts(districts)
+
+
+
+
 
 # %%
